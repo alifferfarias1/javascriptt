c583cd6a64e00a3ea0967f92ce6b6a89195691a8
Aliffer <alifferalvesfarias3@gmail.com>
2023-08-17 19:57:33 -0300

    update

---
+++ aula8/script.js
@@ -0,0 +1,15 @@
+let num1 = prompt("Digite 1 numero: ");
+let num2 = prompt("Digite outro numero: ");
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+
+const resultado = num1 + num2
+
+alert(resultado)
+
+
+confirm('Deseja apagar')
+
+
