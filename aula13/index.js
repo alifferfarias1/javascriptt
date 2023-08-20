5de6695c671fae65d2a01be77ac4d4ca78bd3d50
Aliffer <alifferalvesfarias3@gmail.com>
2023-08-20 14:51:02 -0300

    create a exercicios de Numbers

---
+++ aula13/index.js
@@ -0,0 +1,12 @@
+// Objeto math, um objeto contém metodos em javascript
+
+let num1 = 9.50;
+
+let num2 = Math.ceil(num1); // arrendondando para cima
+console.log(num2);
+
+console.log(Math.floor(num1)); // arrendondando para baixo
+
+console.log(Math.round(num1)); // arrendondando para o mais proximo, caso o num esteja na metade(0.5 ou     0.50) vai para cima
+
+console.log(Math.max(1, 2, 3, 4, 5, 6, 7, -2));
