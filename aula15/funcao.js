a9569ace8a150f9c383816ae107c4eff3196fd40
Aliffer <alifferalvesfarias3@gmail.com>
2023-08-23 14:42:03 -0300

    create a function

---
+++ aula15/funcao.js
@@ -0,0 +1,38 @@
+function saudacao(nome) {
+    return `Bom dia ${nome}`
+}
+
+
+
+const variavel = saudacao('Luiz');
+
+console.log(variavel)
+
+// funcao de soma
+// declarei valores inicialmente para x e y caso, não seja enviado nenhum argumeto para seu parametro        
+function soma(x = 1, y = 7){
+    let soma2 = x + y;
+    return soma2
+}
+resultado = soma(1, 2)
+
+console.log(resultado)
+//poderiamos exibir direto no console.log(soma(1, 2))
+
+console.log(soma(1))
+
+// outro metodos para criar funções
+// funcao anonima
+
+const raiz = function (n) { // pegando a raiz de um numero com a função anonima
+    return n ** 0.5
+};
+
+console.log(raiz(9))
+
+// arrow function
+const raizGrande1 = (n) => { // pegando a raiz de um numero com a função anonima
+    return n ** 0.5
+};
+
+console.log(raizGrande1(2));
