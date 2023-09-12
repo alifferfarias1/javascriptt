e31bbc4647e9105ac591fd6f9663db20de1858bb
Aliffer <alifferalvesfarias3@gmail.com>
2023-09-12 11:17:41 -0300

    operadores.js

---
+++ aula19/operardores.js
@@ -0,0 +1,12 @@
+/*
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual
+== igualdade (valor) ****
+=== igualdade estrita
+!= diferente(valor) ****
+!== diferente estrito
+*/
+
+console.log(10 > 5);
