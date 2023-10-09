8a5dbe59f2a314d730b8f6da203c76a31d9d50b9
Aliffer <alifferalvesfarias3@gmail.com>
2023-10-09 16:02:52 -0300

    update main.js

--- exercicios/exc01/assets/js/main.js
+++ exercicios/exc01/assets/js/main.js
@@ -1,27 +1,43 @@
 function meuEscopo() {
 
-const form = document.querySelector('#form');
-const resultados = document.querySelector('#resultados')
+    const form = document.querySelector('#form');
+    const resultados = document.querySelector('#resultados')
 
-let peso = form.querySelector('#peso');
-let altura = form.querySelector('#altura');
+    let peso = form.querySelector('#peso');
+    let altura = form.querySelector('#altura');
 
 
-const pessoas = [];
+    const pessoas = [];
 
-function recebeEventoForm(evento) {
-    evento.preventDefault();
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
 
-    pessoas.push({
-        peso: peso.value,
-        altura: altura.value
-    })
+        pessoas.push({
+            peso: peso.value,
+            altura: altura.value
+        })
 
-    let imc = peso.value / (altura.value * altura.value)
+        let imc = peso.value / (altura.value * altura.value)
 
-    resultados.innerHTML = `<br> IMC: ${imc.toFixed(2)}</p>  `
 
-}
-form.addEventListener('submit', recebeEventoForm);
+
+        if ((peso.value <= 20 || peso.value >= 480) && (altura.value <= 1.00 || altura >= 2.50)) {
+            resultados.innerHTML = `<br> <p>Ambos invalidos</p> `
+
+        }
+        else if (peso.value <= 20 || peso.value >= 480) {
+            resultados.innerHTML = `<br> <p>Peso invalido</p> `
+
+        } else if (altura.value <= 1.00 || altura >= 2.50) {
+            resultados.innerHTML = `<br> <p>Altura invalida</p> `
+        } else {
+            resultados.innerHTML = `<br> <p>IMC: ${imc.toFixed(2)}</p>  `
+
+
+        }
+
+
+    }
+    form.addEventListener('submit', recebeEventoForm);
 }
 meuEscopo();
