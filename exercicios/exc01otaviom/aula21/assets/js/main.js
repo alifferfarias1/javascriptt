3a3699dd48f682902e41fa00587e208d61067d9d
Aliffer <alifferalvesfarias3@gmail.com>
2023-10-09 22:11:24 -0300

    exercicio solucionado pelo prof

--- exercicios/exc01otaviom/aula21/assets/js/main.js
+++ exercicios/exc01otaviom/aula21/assets/js/main.js
@@ -1,1 +1,28 @@
-alert(1);
+// capturar evento de submit do formulario
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const inpPeso = event.target.querySelector('#peso')
+    const inpAltura = event.target.querySelector('#altura')
+
+    const peso = Number(inpPeso.value);
+    const altura = Number(inpAltura.value);
+    
+
+});
+
+function criaP() {
+    const p = document.createElement('p');
+    return p
+    // p.classList.add('paragrafo-resultado')
+
+}
+
+
+function setResultado (msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+}
