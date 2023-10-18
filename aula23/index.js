6209326878177c9500f28f4f844d75cca7e14571
Aliffer <alifferalvesfarias3@gmail.com>
2023-10-18 16:11:52 -0300

    switch case

---
+++ aula23/index.js
@@ -0,0 +1,64 @@
+const data = new Date('1987-04-26 00:00');
+
+let diaSemana = data.getDay();
+let diaSemanaText;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaText = 'Domingo';
+        break;
+    case 1:
+        diaSemanaText = 'Segunda';
+        break;
+    case 2:
+        diaSemanaText = 'Terça';
+        break;
+    case 3:
+        diaSemanaText = 'Quarta';
+        break;
+    case 4:
+        diaSemanaText = 'Quinta';
+        break;
+    case 5:
+        diaSemanaText = 'Sexta';
+        break;
+    case 6:
+        diaSemanaText = 'Sabado';
+        break;
+    default:
+        diaSemanaText = '';
+}
+
+console.log(diaSemana, diaSemanaText);
+
+
+
+
+
+
+
+
+
+
+// if (diaSemana === 0) {
+//     diaSemanaText = 'Domingo';
+// }else if (diaSemana === 1){
+//     diaSemanaText = 'Segunda';
+// }
+// else if (diaSemana === 2){
+//     diaSemanaText = 'Terça';
+// }
+// else if (diaSemana === 3){
+//     diaSemanaText = 'Quarta';
+// }
+// else if (diaSemana === 4){
+//     diaSemanaText = 'Quinta';
+// }
+// else if (diaSemana === 5){
+//     diaSemanaText = 'Sexta';
+// }
+// else if (diaSemana === 6){
+//     diaSemanaText = 'Sabado';
+// } else{
+//     diaSemanaText = '';
+// }
