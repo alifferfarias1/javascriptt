853f0887896f72db14b358c1073b499b763c4f2b
Aliffer <alifferalvesfarias3@gmail.com>
2023-10-27 18:04:52 -0300

    dom e a arvore

--- aula25/main.js
+++ aula25/main.js
@@ -1,7 +1,7 @@
 // ...rest ... spread
 
 const numeros = [[1, 2, 3],[4, 5, 6],[7, 8, 9],];
-console.log(numeros[1][0])
+console.log(numeros[0][0])
 
 
 
@@ -16,6 +16,7 @@ console.log(numeros[1][0])
 
 
 
-// const [um, dois, tres, , cinco, ...resto] = numeros; // com um espaço em branco podemos pular alguma valor
+const [um, dois, ...resto] = numeros; // com um espaço em branco podemos pular alguma valor
+console.log(um, dois, resto)
 // console.log(um, dois, tres, cinco);
 // console.log(resto);
