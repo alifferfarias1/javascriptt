7ea93b4cd2b98d94883bd1bb3fc3fb77f0bc621f
Aliffer <alifferalvesfarias3@gmail.com>
2023-10-29 17:53:09 -0300

    aula 30

---
+++ aula30/script.js
@@ -0,0 +1,32 @@
+// While e do While
+
+const nome = 'Luiz'
+let controle = 0;
+while (controle < 10) { // semelhante ao enquanto faça do visualg checa a condição antes de entrar no loop
+    console.log(controle)
+    controle++
+}
+
+
+function random(min, max){
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+const min = 1
+const max = 50
+let rand = random(min, max);
+
+while (rand !== 10){
+    rand = random(min, max);
+    console.log(rand)
+
+}
+
+// 
+
+console.log('=========================================================')
+
+do {
+    rand = random(min, max); //executa pelo menos uma vez, semelhante ao repita até do Visualg
+    console.log(rand)
+} while (rand !== 10);
